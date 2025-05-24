73536a079fc8d030b6ca18737fcd6cea8be9dd45
frank <ndungufrank01@gmail.com>
2025-05-24 08:10:06 +0300

    add: Implementing table ui

--- resources/js/pages/contacts/index.tsx
+++ resources/js/pages/contacts/index.tsx
@@ -1,5 +1,6 @@
 import { Button } from '@/components/ui/button';
 
+import { Table, TableBody, TableCaption, TableCell, TableHead, TableHeader, TableRow } from '@/components/ui/table';
 import AppLayout from '@/layouts/app-layout';
 import { type BreadcrumbItem } from '@/types';
 import { Head, Link } from '@inertiajs/react';
@@ -28,6 +29,27 @@ export default function Contacts() {
                         <Plus className="h-4 w-4" />
                         Add Contact
                     </Button>
+                </div>
+                <div className="flex flex-col gap-4">
+                    <Table>
+                        <TableCaption>A list of your recent invoices.</TableCaption>
+                        <TableHeader>
+                            <TableRow>
+                                <TableHead className="w-[100px]">Invoice</TableHead>
+                                <TableHead>Status</TableHead>
+                                <TableHead>Method</TableHead>
+                                <TableHead className="text-right">Amount</TableHead>
+                            </TableRow>
+                        </TableHeader>
+                        <TableBody>
+                            <TableRow>
+                                <TableCell className="font-medium">INV001</TableCell>
+                                <TableCell>Paid</TableCell>
+                                <TableCell>Credit Card</TableCell>
+                                <TableCell className="text-right">$250.00</TableCell>
+                            </TableRow>
+                        </TableBody>
+                    </Table>
                 </div>
             </div>
         </AppLayout>
